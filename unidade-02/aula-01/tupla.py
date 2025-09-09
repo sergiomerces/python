70cc17ae3d81534e6bb60d224602b007ba8e008a
sergiomerces <sergio.merces@gmail.com>
2025-09-09 02:10:11 -0300

    correção

--- unidade-02/aula-01/tupla.py
+++ unidade-02/aula-01/tupla.py
@@ -4,7 +4,7 @@ tupla2 = ('a', 'b', 'c')
 
 vogais = ('a', 'e', 'i', 'o', 'u')
 
-print(f"Tipo de objeto voagais = {type(vogais)}")
+print(f"Tipo de objeto vogais = {type(vogais)}")
 
 for p, x in enumerate(vogais):
     print(f"Posição = {p}, Valor = {x}")
