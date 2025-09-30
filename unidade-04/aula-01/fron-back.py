bd6dfe3b9b661e9dc75406d0573e4e7a0b571f55
sergiomerces <sergio.merces@gmail.com>
2025-09-30 00:21:12 -0300

    desenvolvendo para web com python

---
+++ unidade-04/aula-01/fron-back.py
@@ -0,0 +1,19 @@
+# Criando uma página HTML usando Python
+
+html_code =
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Exemplo de Front-end com Python</title>
+</head>
+<body>
+  <h1>Olá, mundo!</h1>
+  <p>Esta é uma página web criada usando Python no Google Colab.</p>
+</body>
+</html>
+
+# Exibindo a página HTML
+from IPython.display import HTML
+
+HTML(html_code)
